43bb5bf13b10ac8d60eead6618920cc6272def0b
Jonas Schell <jonasschell@ocupe.org>
2026-02-09 22:25:57 +0100

    Remove bl_info from __init__.py and add blender_manifest.toml for project metadata and configuration.

--- __init__.py
+++ __init__.py
@@ -2,18 +2,6 @@ from . import ui
 from . import projector
 from . import operators
 
-bl_info = {
-    "name": "Projector",
-    "author": "Jonas Schell",
-    "description": "Easy Projector creation and modification.",
-    "blender": (2, 81, 0),
-    "version": (2024, 1, 1),
-    "location": "3D Viewport > Add > Light > Projector",
-    "category": "Lighting",
-    "wiki_url": "https://github.com/Ocupe/Projectors/wiki",
-    "tracker_url": "https://github.com/Ocupe/Projectors/issues"
-}
-
 
 def register():
     projector.register()
